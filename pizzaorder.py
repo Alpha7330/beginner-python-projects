a8d4f510bff39314b2493cfee0b74d706d518d32
Alpha7330 <91137815+Alpha7330@users.noreply.github.com>
2024-05-05 16:44:15 +0530

    Update pizzaorder.py

--- pizzaorder.py
+++ pizzaorder.py
@@ -2,8 +2,6 @@ print("Thank you for choosing Python Pizza Deliveries!")
 size = input("enter size\n") # What size pizza do you want? S, M, or L
 add_pepperoni = input("enter Y or N\n") # Do you want pepperoni? Y or N
 extra_cheese = input("enter Y or N\n") # Do you want extra cheese? Y or N
-# 🚨 Don't change the code above 👆
-# Write your code below this line 👇
 s=size
 a=add_pepperoni
 e=extra_cheese
