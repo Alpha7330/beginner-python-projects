667fe03080f78166b93fa1d68a6167d13b3e7273
Alpha7330 <91137815+Alpha7330@users.noreply.github.com>
2024-05-09 13:00:22 +0530

    Add files via upload

---
+++ paint_calc.py
@@ -0,0 +1,9 @@
+import math
+def paint_calc(height,width,cover):
+   cans=(height*width)/cover
+   cans=math.ceil(cans)
+   print(f"You'll need {cans} cans of paint.")
+test_h = int(input()) # Height of wall (m)
+test_w = int(input()) # Width of wall (m)
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
---
+++ prime_checker.py
@@ -0,0 +1,17 @@
+# Write your code below this line 👇
+def prime_checker(number):
+  for i in range(2,number):
+    if number==2:
+      print("It's a prime number.")
+      return
+    elif  number%i==0:
+      print("It's not a prime number.")
+      return
+    
+  print("It's a prime number.")
+      
+# Write your code above this line 👆
+    
+#Do NOT change any of the code below👇
+n = int(input()) # Check this number
+prime_checker(number=n)
